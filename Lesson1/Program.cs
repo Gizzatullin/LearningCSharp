388cb95685252fbd923746768fab279783a62188
Gizzatullin <g-rus-i@mail.ru>
2022-07-10 12:11:32 +0400

    Beginning

---
+++ Lesson1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word!");
+        }
+    }
+}
