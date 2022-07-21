3355524986485a503a6d9fe1347935c54568bb1f
Gizzatullin <g-rus-i@mail.ru>
2022-07-21 15:32:52 +0400

    ChangeCODe(del-goto change-if in blok in ChekWin)

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -18,89 +18,112 @@ namespace Lesson2
             Console.WriteLine("-------------");
         }
 
-        static void CheckWinner(Char[] Pole, ref int winX, ref int winY)
+        static void CheckWinner(Char[] Pole, ref int winX, ref int winY, int[] WinComb)
         {
-            if (Pole[0] == 'X' && Pole[1] == 'X' && Pole[2] == 'X') winX = 1;
-            if (Pole[3] == 'X' && Pole[4] == 'X' && Pole[5] == 'X') winX = 1;
-            if (Pole[6] == 'X' && Pole[7] == 'X' && Pole[8] == 'X') winX = 1;
-            if (Pole[0] == 'X' && Pole[3] == 'X' && Pole[6] == 'X') winX = 1;
-            if (Pole[1] == 'X' && Pole[4] == 'X' && Pole[7] == 'X') winX = 1;
-            if (Pole[2] == 'X' && Pole[5] == 'X' && Pole[8] == 'X') winX = 1;
-            if (Pole[0] == 'X' && Pole[4] == 'X' && Pole[8] == 'X') winX = 1;
-            if (Pole[2] == 'X' && Pole[4] == 'X' && Pole[6] == 'X') winX = 1;
-
-            if (Pole[0] == 'Y' && Pole[1] == 'Y' && Pole[2] == 'Y') winY = 1;
-            if (Pole[3] == 'Y' && Pole[4] == 'Y' && Pole[5] == 'Y') winY = 1;
-            if (Pole[6] == 'Y' && Pole[7] == 'Y' && Pole[8] == 'Y') winY = 1;
-            if (Pole[0] == 'Y' && Pole[3] == 'Y' && Pole[6] == 'Y') winY = 1;
-            if (Pole[1] == 'Y' && Pole[4] == 'Y' && Pole[7] == 'Y') winY = 1;
-            if (Pole[2] == 'Y' && Pole[5] == 'Y' && Pole[8] == 'Y') winY = 1;
-            if (Pole[0] == 'Y' && Pole[4] == 'Y' && Pole[8] == 'Y') winY = 1;
-            if (Pole[2] == 'Y' && Pole[4] == 'Y' && Pole[6] == 'Y') winY = 1;
+            for (int i = 0; i < 22; i = i+3)
+            {
+                if (Pole[WinComb[i]] == 'X' && Pole[WinComb[i + 1]] == 'X' && Pole[WinComb[i + 2]] == 'X') winX = 1;
+                if (Pole[WinComb[i]] == 'Y' && Pole[WinComb[i + 1]] == 'Y' && Pole[WinComb[i + 2]] == 'Y') winY = 1;
+            }
         }
 
 
 
         static void Main(string[] args)
         {
-        bg: Char[] Pole = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-            Char[] Name = { 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X' };
-            int NumName = 0;
-            int winX = 0;
-            int winY = 0;
+            int NewGame = 0;
+            int[] WinComb = {0,1,2,3,4,5,6,7,8,0,3,6,1,4,7,2,5,8,0,4,8,2,4,6};
 
-
-            Console.WriteLine("Игра «Крестики-Нолики» (TicTacToe)");
-            Console.WriteLine("----------------------------------");
-            Console.WriteLine();
-            Picture(Pole);
-
-
-            for (NumName = 0; NumName < 9; NumName++)
+            do
             {
+                Char[] Pole = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                Char[] Name = { 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X' }; 
+                int NumName = 0;
+                int winX = 0;
+                int winY = 0;
+                int GameBreak = 0;
 
-            m1: Console.Write("Ход игрока " + Name[NumName] + ":");
-                string input = Console.ReadLine();
-                if (input != "1" && input != "2" && input != "3" && input != "4" && input != "5" && input != "6" && input != "7" && input != "8" && input != "9")
-                {
-                    Console.WriteLine("Вы нажали не правильную клавишу! Выберите значение от 1 до 9");
-                    goto m1;
-                }
-                else { }
-
-                int a = Convert.ToInt32(input);
-                string b = Convert.ToString(Pole[a - 1]);
-                switch (b)
-                {
-                    case "X":
-                        Console.WriteLine("Данное поле уже занято X! Выберите другое поле");
-                        goto m1;
-                    case "Y":
-                        Console.WriteLine("Данное поле уже занято Y! Выберите другое поле");
-                        goto m1;
-                    default:
-                        Pole[a - 1] = Name[NumName];
-                        break;
-                }
-                
+                Console.WriteLine("Игра «Крестики-Нолики» (TicTacToe)");
+                Console.WriteLine("----------------------------------");
                 Console.WriteLine();
                 Picture(Pole);
-               
-                CheckWinner(Pole, ref winX, ref winY);
 
-                if (winX == 1 || winY == 1)
+                do
                 {
-                    Console.WriteLine("ПОЗДРАВЛЯЮ !!! Выйграл игрок " + Name[NumName]);
-                    goto end;
+                    int ErrorInput;
+
+                    do
+                    {
+                        ErrorInput = 0;
+                        Console.Write("Ход игрока " + Name[NumName] + ":");
+                        string input = Console.ReadLine();
+
+
+                        int index1 = 0;
+                        for (int i = 1; i <= 9; i++)
+                        {
+                            string cf = Convert.ToString(i);
+                            if (input == cf) { index1 = 1; }
+                        }
+
+                        if (index1 == 1)
+                        {
+                            int a = Convert.ToInt32(input);
+                            string b = Convert.ToString(Pole[a - 1]);
+                            switch (b)
+                            {
+                                case "X": { index1 = 2; break; }
+                                case "Y": { index1 = 3; break; }
+                                default: { Pole[a - 1] = Name[NumName]; break; }
+                            }
+                        }
+
+                        if (index1 == 0)
+                        {
+                            Console.WriteLine("Вы нажали не правильную клавишу! Выберите значение от 1 до 9");
+                            ErrorInput = 1;
+                        }
+                        if (index1 == 2)
+                        {
+                            Console.WriteLine("Данное поле уже занято X! Выберите другое поле");
+                            ErrorInput = 1;
+                        }
+                        if (index1 == 3)
+                        {
+                            Console.WriteLine("Данное поле уже занято Y! Выберите другое поле");
+                            ErrorInput = 1;
+                        }
+                    }
+                    while (ErrorInput == 1);
+
+
+                    Console.WriteLine();
+                    Picture(Pole);
+
+
+
+                    CheckWinner(Pole, ref winX, ref winY, WinComb);
+
+                    if (winX == 1 || winY == 1)
+                    {
+                        Console.WriteLine("ПОЗДРАВЛЯЮ !!! Выйграл игрок " + Name[NumName]);
+                        GameBreak++;
+                    }
+                    
+                    NumName++;
+                    if (NumName == 9 && winX == 0 && winY == 0)
+                    {
+                        Console.WriteLine("НИЧЬЯ !!!");
+                        GameBreak++;
+                    }
                 }
+                while (GameBreak == 0);
 
+                Console.WriteLine("Желаете ещё сыграть (y/n)?");
+                string choise = Console.ReadLine();
+                if (choise == "n") NewGame = 1;
             }
-            Console.WriteLine("НИЧЬЯ !!!");
-
-       end:Console.WriteLine("Желаете ещё сыграть (y/n)?");
-            string choise = Console.ReadLine();
-            if (choise == "y") goto bg;
-
+            while (NewGame == 0);
 
         }
 
