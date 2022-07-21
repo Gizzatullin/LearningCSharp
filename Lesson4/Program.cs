74bffe7080d7a0cc60f07d998f934c0cd5bd7f8d
Gizzatullin <g-rus-i@mail.ru>
2022-07-21 23:09:37 +0400

    Add Lesson4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Phonebook
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                bool FlagExit = true;
+                string FileName;
+                
+                do
+                {   
+                    Console.WriteLine("---Выберите действие с телефонной книгой---");
+                    Console.WriteLine("-                                         -");
+                    Console.WriteLine("-  1. Создать новую запись.               -");
+                    Console.WriteLine("-  2. Прочитать все записи.               -");
+                    Console.WriteLine("-  3. Обновить запись.                    -");
+                    Console.WriteLine("-  4. Удалить запись.                     -");
+                    Console.WriteLine("-  5. Выйти из программы.                 -");
+                    Console.WriteLine("-------------------------------------------");
+
+                    ConsoleKeyInfo ChoiceMenu = Console.ReadKey(true);
+                    
+                    switch (ChoiceMenu.Key)
+                    {
+                        case ConsoleKey.NumPad1: Creat(); break;
+                        case ConsoleKey.NumPad2: Read(); break;
+                        case ConsoleKey.NumPad3: Update(); break;
+                        case ConsoleKey.NumPad4: Delete(); break;
+                        case ConsoleKey.NumPad5: FlagExit = false; break;
+                        default: break;
+                    }
+                }
+                while (FlagExit == true);
+        
+
+            }
+
+            static void Creat()
+            {
+
+            }
+
+            static void Read()
+            {
+
+            }
+
+            static void Update()
+            {
+
+            }
+
+            static void Delete()
+            {
+
+            }
+
+
+        }
+    }
+
+
+    class Abonent
+    {
+        public int NumberPhone;
+        public string NamePhone;
+
+
+    }
+}
---
+++ Lesson4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lesson4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson4")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("eb670b19-2b48-4ff3-8e29-f19f89b885fe")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
