3894186542d3e9dd82487a2a8b05efab6a57db66
Gizzatullin <g-rus-i@mail.ru>
2022-07-22 22:16:19 +0400

    Change Lesson4 after lesson 22.07

---
+++ Lesson4/Phonebook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    internal class Phonebook
+    {
+
+
+
+    }
+}
---
+++ Lesson4/Subscriber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    internal class Subscriber
+    {
+        private string NumberPhone;
+        private string NamePhone;
+
+
+    }
+}
