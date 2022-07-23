1bca9272c4a2231c412b3cf60ca6c4d8f4f3d15b
Gizzatullin <g-rus-i@mail.ru>
2022-07-23 22:52:41 +0400

    Create Subscriber.cs

--- Lesson4/Subscriber.cs
+++ Lesson4/Subscriber.cs
@@ -1,16 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Lesson4
 {
     internal class Subscriber
     {
-        private string NumberPhone;
-        private string NamePhone;
+        /// <summary>
+        /// Создал поле "Номер телефона" как строковую с возможностью только считывать   
+        /// </summary>
+        private readonly string NumberPhone;
 
 
+        /// <summary>
+        /// Создал поле "Имя" как строковую с возможностью только считывать   
+        /// </summary>
+        private readonly string NamePhone;
+
+
+        /// <summary>
+        /// Создал свойство "Subscriber", чтобы совершать с ним логические операции
+        /// </summary>
+        /// <param name="Numberphone"></param>
+        /// <param name="Namephone"></param>
+        public Subscriber(string Numberphone, string Namephone)
+        {
+            this.NumberPhone = Numberphone;
+            this.NamePhone = Namephone;
+        }
+        
     }
 }
