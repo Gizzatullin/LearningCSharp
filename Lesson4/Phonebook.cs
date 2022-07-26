400b092e112fd1323429025412ebea939271eb4e
Gizzatullin <g-rus-i@mail.ru>
2022-07-26 23:05:20 +0400

    Upgrade Lesson4.csproj

--- Lesson4/Phonebook.cs
+++ Lesson4/Phonebook.cs
@@ -9,6 +9,50 @@ namespace Lesson4
     internal class Phonebook
     {
 
+        /// <summary>
+        /// Method of adding a contact
+        /// </summary>
+        /// <param name="Contacts"></param>
+        public void AddContact(List<string> Contacts)
+        {
+            Console.WriteLine("\n- Добавляем контакт -");
+
+            Console.Write("Введите телефонный номер:");
+            string Nomer = Console.ReadLine();
+            Console.Write("Введите имя:");
+            string Name = Console.ReadLine();
+
+            Contacts.Add(Nomer + "\t" + Name);
+        }
+
+        public void CorrectContact()
+        {
+
+        }
+
+        /// <summary>
+        /// Contact removal method
+        /// </summary>
+        /// <param name="Contacts"></param>
+        public void DelContact(List<string> Contacts)
+        {
+            Console.WriteLine("\n- Удаляем контакт -");
+
+            Console.Write("Введите имя, контакт которого вы желаете удалить:");
+            string Name = Console.ReadLine();
+
+            int index = Contacts.FindIndex(x => x.Contains(Name));
+            Contacts.RemoveAt(index);
+        }
+
+
+
+
+
+
+
+
+
 
 
     }
--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -1,66 +1,108 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
 
 namespace Lesson4
-{    
+{
     internal class Program
     {
+        const string FileName = "phonebook.txt";
+
         static void Main(string[] args)
         {
-            Menu();
-            
-            
+            List<string> Contacts = new List<string>();
+
+            ReadPhonebook(Contacts);
+            Menu(Contacts);
+            WritePhonebook(Contacts);
         }
 
 
-        static void Menu()
+        /// <summary>
+        /// Method of reading a text file and getting a list of contacts
+        /// </summary>
+        static void ReadPhonebook(List<string> Contacts)
+        {
+            string FilePath = Path.Combine(Environment.CurrentDirectory, FileName);
+            string [] lines = File.ReadAllLines(FilePath);
+            
+            for (int i = 0; i < lines.Length; i++)
+            {   Contacts.Add(lines[i]); 
+            }                        
+        }
+
+        /// <summary>
+        /// Method of writing a contacts in txt-file
+        /// </summary>
+        /// <param name="Contacts"></param>
+        static void WritePhonebook(List<string> Contacts)
+        {
+            string FilePath = Path.Combine(Environment.CurrentDirectory, FileName);
+            File.WriteAllLines(FilePath, Contacts);
+        }
+
+
+        /// <summary>
+        /// Phonebook and control interface output method
+        /// </summary>
+        static void Menu(List<string> Contacts)
         {
             bool FlagExit = true;
-            
+                        
             do
-            {
+            {   
+                Console.WriteLine("\n-Телефонная книжка- ");
+                foreach (string Contact in Contacts) 
+                {
+                    Console.WriteLine(Contact);
+                   }
+                Console.WriteLine();
+                
+                
                 Console.WriteLine("---Выберите действие с телефонной книгой---");
                 Console.WriteLine("-                                         -");
                 Console.WriteLine("-  1. Создать новую запись.               -");
-                Console.WriteLine("-  2. Прочитать все записи.               -");
-                Console.WriteLine("-  3. Обновить запись.                    -");
-                Console.WriteLine("-  4. Удалить запись.                     -");
-                Console.WriteLine("-  5. Выйти из программы.                 -");
+                Console.WriteLine("-  2. Обновить запись.                    -");
+                Console.WriteLine("-  3. Удалить запись.                     -");
+                Console.WriteLine("-  4. Выйти из программы.                 -");
                 Console.WriteLine("-------------------------------------------");
 
                 ConsoleKeyInfo ChoiceMenu = Console.ReadKey(true);
-
                 switch (ChoiceMenu.Key)
                 {
-                    case ConsoleKey.NumPad1: Creat(); break;
-                    case ConsoleKey.NumPad2: Read(); break;
-                    case ConsoleKey.NumPad3: Update(); break;
-                    case ConsoleKey.NumPad4: Delete(); break;
-                    case ConsoleKey.NumPad5: FlagExit = false; break;
+                    case ConsoleKey.NumPad1: Creat(Contacts); break;
+                    case ConsoleKey.NumPad2: Update(); break;
+                    case ConsoleKey.NumPad3: Delete(Contacts); break;
+                    case ConsoleKey.NumPad4: FlagExit = false; break;
                     default: break;
                 }
             }
             while (FlagExit == true);
         }
 
-
-        static void Creat()
-        {
-
+        /// <summary>
+        /// Calling the Phonebook class to add a contact
+        /// </summary>
+        /// <param name="Contacts"></param>
+        static void Creat(List<string> Contacts)
+        {   Phonebook phonebookInstance = new Phonebook();
+            phonebookInstance.AddContact(Contacts);
         }
-
-        static void Read()
-        {
-
-        }
-
+        
+        
         static void Update()
-        {
-
+        {   Phonebook phonebookInstance = new Phonebook();
+            phonebookInstance.CorrectContact();
         }
 
-        static void Delete()
-        {
-
+        /// <summary>
+        /// Calling the Phonebook class to remove a contact
+        /// </summary>
+        /// <param name="Contacts"></param>
+        static void Delete(List<string> Contacts)
+        {   Phonebook phonebookInstance = new Phonebook();
+            phonebookInstance.DelContact(Contacts);
         }
 
     }
