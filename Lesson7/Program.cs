e134d60263519aca47eb6455dc5a19c04042b36d
Gizzatullin <g-rus-i@mail.ru>
2022-08-04 12:35:11 +0400

    Upgrade Lesson7 to 04.08

--- Lesson7/Program.cs
+++ Lesson7/Program.cs
@@ -7,8 +7,38 @@ namespace Lesson7
     /// </summary>
     internal class Program
     {
+
+        /*public class Exception
+        {
+                
+        }*/
+
+
         static void Main(string[] args)
         {
+
+            try
+            {
+                Console.WriteLine("Введите какую-нибудь информацию: ");
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    throw new Exception("Вами введена пустая строка");
+                }
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Введена пустая срока");
+            }
+
+            finally
+            {
+                Console.WriteLine("Конец программы!");
+            }
+            
+            Console.ReadKey();
+
         }
     }
 }
